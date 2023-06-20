4c022ac3db674606b9b2d7f03df3b4caedb7b9e3
wkdwns0501 <britain0501@naver.com>
2023-06-20 11:17:28 +0900

    "" 활용

--- day02/src/basic/Hello.java
+++ day02/src/basic/Hello.java
@@ -1,4 +1,4 @@
-package day02;//이 파일의 위치
+package basic;//이 파일의 위치
 
 public class Hello {//Hello 클래스(=파일명)
 	public static void main(String[] args) {//main 메소드(고정)
--- day02/src/basic/Information.java
+++ day02/src/basic/Information.java
@@ -1,7 +1,6 @@
-package day02;
+package basic;
 
 public class Information {
-
 	public static void main(String[] args) {
 		System.out.println("과정명 : 공공데이터 융합 웹 애플리케이션 개발자 양성과정4");
 		System.out.println("강사명 : 황인빈 강사님");

6350fd4157c9224045630f12b314dafafda294dd
wkdwns0501 <britain0501@naver.com>
2023-06-20 10:48:03 +0900

    명령어 설명

---
+++ day02/src/day02/Hello.java
@@ -0,0 +1,9 @@
+package day02;//이 파일의 위치
+
+public class Hello {//Hello 클래스(=파일명)
+	public static void main(String[] args) {//main 메소드(고정)
+			System.out.println("Hello!");//출력 명령
+			System.out.println(100);
+	}
+
+}

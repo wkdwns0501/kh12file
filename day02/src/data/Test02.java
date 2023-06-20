f086b09ee3403207724ca82ef4dea3ea929cca56
wkdwns0501 <britain0501@naver.com>
2023-06-20 12:40:33 +0900

    중국집 가격 문제

---
+++ day02/src/data/Test02.java
@@ -0,0 +1,9 @@
+package data;
+
+public class Test02 {
+		public static void main(String[] args) {
+			System.out.println("자장면 : 7000원");
+			System.out.println("짬뽕 : 8000원");
+			System.out.println("자장면 2그릇과 짬뽕 3그릇의 가격은 " + (7000*2 + 8000*3) +"원 입니다.");
+		}
+}

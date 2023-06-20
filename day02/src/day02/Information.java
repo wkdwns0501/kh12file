58e209c58825b2bce1495d30344dfe828ce4a065
wkdwns0501 <britain0501@naver.com>
2023-06-20 10:53:54 +0900

    과정, 강사, 학생 설명

---
+++ day02/src/day02/Information.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class Information {
+
+	public static void main(String[] args) {
+		System.out.println("과정명 : 공공데이터 융합 웹 애플리케이션 개발자 양성과정4");
+		System.out.println("강사명 : 황인빈 강사님");
+		System.out.println("학생명 : 장준");
+	}
+
+}

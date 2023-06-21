8e841b9ba4299a34eb7503656099f54923959ca9
wkdwns0501 <britain0501@naver.com>
2023-06-21 10:25:53 +0900

    할인 문제

---
+++ day03/src/data/Test14.java
@@ -0,0 +1,23 @@
+package data;
+
+public class Test14 {
+	public static void main(String[] args) {
+		//할인 금액 구하기
+		//(Q) 300만원짜리 컴퓨터를 20% 할인하면 얼마인가?
+		
+		//입력
+		int price = 3000000;
+		int rate = 20;
+		
+		//계산
+		//- (주의) 나누기를 먼저하면 손실이 발생할 수 있다
+		int discount = price * rate / 100;
+		//int discount = price / 100 * rate ; //문제가 있음
+		//System.out.println(discount);
+		
+		int pay = price - discount;
+		
+		//출력
+		System.out.println(pay);
+	}
+}

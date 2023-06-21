cd993b7de440bfc933684f595a1b0a634b187338
wkdwns0501 <britain0501@naver.com>
2023-06-21 09:49:50 +0900

    1+1문제 해설 추가

--- day02/src/data/Test12.java
+++ day02/src/data/Test12.java
@@ -6,14 +6,21 @@ public class Test12 {
 		//5개를 샀을 때 다음 항목을 계산하여 출력하세요
 		//무료 처리되는 개수, 최종 구매 금액
 		//참고로 아르바이트생은 절대로 한개 더 가져오라고 하지 않습니다
+		//완성을하면 검증을 해봐라 (숫자만 바꿔보기)
 		
-		int buyGimBap = 5;  
+		//입력
+		int count = 5;  
 		int price = 1400;
 		
-		int freeGimbap = buyGimBap / 2;
-		int totalPrice = (buyGimBap - freeGimbap) * price;
+		//계산
+		int free= count / 2;
+		//System.out.println(free);
+		int pay = count - free;
+		//System.out.println(pay);
+		int totalPrice = pay * price;
 		
-		System.out.println(freeGimbap);
+		//출력
+		System.out.println(free);
 		System.out.println(totalPrice);
 	}
 }

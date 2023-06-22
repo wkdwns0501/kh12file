e25cb01ea6b626b428f51275e308318903b6aa82
wkdwns0501 <britain0501@naver.com>
2023-06-22 16:03:00 +0900

    조건 (Condition) - if문

---
+++ day04/src/condition/Test01.java
@@ -0,0 +1,19 @@
+package condition;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//조건(Condition)
+		
+		int number = 6;
+		
+		//if(number가 홀수라면)
+		if(number % 2 != 0){
+			System.out.println("홀수");
+		}
+		//if(number가 짝수라면)
+		//if(number % 2 == 0){
+		else {
+			System.out.println("짝수");
+		}
+	}
+}

dac08a48bfa5f38e62b138dd561a465ea7733219
wkdwns0501 <britain0501@naver.com>
2023-06-22 12:52:09 +0900

    출력 설명

--- day04/src/data4/Test02.java
+++ day04/src/data4/Test02.java
@@ -6,7 +6,7 @@ public class Test02 {
 		// - 문자가 모여있는 형태의 데이터
 		// - 참조형 데이터(reference type)
 		// - 참조형 데이터는 주문제작 방식으로 처리된다 (복잡한데이터)
-		// - 참조변수(ex : 리모컨)가 존재하며 기능(버튼)이 제공된다
+		// - 참조변수(ex : 리모컨)가 존재하며 기능(버튼)이 제공된다  
 		
 		String a = "hello";
 		System.out.println(a);

33e01b487b750d19c4747c4ba227ce48da12585d
wkdwns0501 <britain0501@naver.com>
2023-06-24 01:59:19 +0900

    day05 과제 오류 수정 및 정리

--- day05/src/loop/Test04.java
+++ day05/src/loop/Test04.java
@@ -4,13 +4,13 @@ public class Test04 {
 	public static void main(String[] args) {
 		//1부터 99 사이에 5가 포함되어 있는 숫자만 필터링하여 출력
 		
-		for (int i = 1 ; i <=99 ; i++) {
+		for (int i = 1 ; i <= 99 ; i++) {
 			int ten = i / 10;
 			int one = i % 10;
+			
 			if (ten == 5 || one == 5) {
 				System.out.println(i);
 			}
 		}
-		
 	}
 }

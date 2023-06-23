1ba4cac5be7ec508d457c4c06c9e36505e70a4b6
wkdwns0501 <britain0501@naver.com>
2023-06-23 17:15:12 +0900

    반복 예제

---
+++ day05/src/loop/Test01.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//반복 (loop)
+		// - (Q) Hello를 화면에 10번 출력하세요
+		
+		//for (10번)							//i++ , i += 1 , i = i+1     다같은 말이다
+		for(int i=0 ; i <10 ; i++) {			//for(시작 ; 범위 ; 증감)
+			System.out.println("Hello");
+		}
+	}
+}

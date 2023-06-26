cf68b8fc04d9a2eb7d06284362e9ad1b58beed81
wkdwns0501 <britain0501@naver.com>
2023-06-26 10:12:06 +0900

    반복문의 다양한 범위

--- day06/src/loop/Test06.java
+++ day06/src/loop/Test06.java
@@ -1,5 +1,24 @@
 package loop;
 
 public class Test06 {
-
+	public static void main(String[] args) {
+		//반복 범위의 변화를 줘서 다양한 반복을 구현
+		
+		//(Q) 10부터 0까지 숫자가 줄어들며 출력하도록 구현
+		
+		for (int i=10 ; i >= 0 ; i--) { // 반복문의 실행순서 int i=10 -> i >= 0 -> System.out.println(i); -> i--
+			System.out.println(i);
+		}
+		
+		//(Q) 10분의 1씩 줄어들며 출력하는 반복문 구현
+		for(int i =12345 ; i>0 ; i/= 10) { 
+			System.out.println(i);
+			//System.out.println(i%10);// 오른쪽 한자리씩 출력
+		}
+		
+		//(Q) 두배씩 늘어나는 반복문 구현
+		for (int i=1 ; i<= 1000 ; i*=2) {
+			System.out.println(i);
+		}
+	}
 }

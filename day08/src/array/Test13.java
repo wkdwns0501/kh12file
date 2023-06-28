7cb0c353002a44a951f94f825d276f4b793c4915
wkdwns0501 <britain0501@naver.com>
2023-06-28 18:20:33 +0900

    day08 과제 배열 (랜덤카드섞기) 문제

---
+++ day08/src/array/Test13.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.Random;
+
+public class Test13 {
+	public static void main(String[] args) {
+		//이전 문제와 동일한 데이터 또는 어떠한 데이터든 관계 없이 준비하고 
+		//배열의 모든 데이터를 랜덤으로 섞는 코드를 구현 (cf:카드섞기)
+		//1. 모든 자리는 1번씩 다른 위치와 바뀌어야 한다
+		//2. 어디랑 섞일지 대상 위치는 모르며 같은 위치일 수도 있다
+		//3. 랜덤으로 정해진 위치와 현재 위치의 데이터를 바꾸도록 구현
+		Random r = new Random();
+		int []card = new int[] {30,50,20,10,40};
+		
+		for (int i = 0 ; i < card.length ; i++) {
+			int num = r.nextInt(5);
+			int backup = card[i];
+			card[i] = card[num];
+			card[num] = backup;
+		}
+		for (int i = 0 ; i < card.length ; i++) {
+			System.out.println(card[i]);
+		}
+	}
+}

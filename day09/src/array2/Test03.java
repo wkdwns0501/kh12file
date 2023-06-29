9fe54bab3f7ac49d957ce357cf75b21782bc24c3
wkdwns0501 <britain0501@naver.com>
2023-06-29 14:29:24 +0900

    정렬 공식

---
+++ day09/src/array2/Test03.java
@@ -0,0 +1,15 @@
+package array2;
+
+import java.util.Arrays;
+
+public class Test03 {
+	public static void main(String[] args) {
+		//나중에 배울 정렬 공식이다
+		
+		int[] data = new int[] {20, 10, 30, 40, 50};
+		
+		Arrays.sort(data); //Crtl 누르고 sort 클릭하면 설명이 보인다
+		
+		System.out.println(Arrays.toString(data));
+	}
+}

d6ecc02da120da8f7eb6c187d6925144d9744535
wkdwns0501 <britain0501@naver.com>
2023-06-29 17:20:38 +0900

    회차별 주사위 정보 분석 문제 (내 풀이)

--- day09/src/array3/Test05.java
+++ day09/src/array3/Test05.java
@@ -8,23 +8,23 @@ public class Test05 {
 		//(1) 회차별 주사위 던진 결과를 출력하시고 
 		//(2) 모든 주사위 값의 평균을 출력하세요
 		
-		//회차의 평균이 안나옴...
-		
 		int[][] diceList = new int[6][5];
 		Random r = new Random();
 		int count = 0;
 		int total = 0;
-		int roundTotal = 0;
+		int[] totalList = new int[diceList.length];
+		float[] avgList = new float[diceList.length];
 		for(int i = 0 ; i < diceList.length ; i++) {
-			System.out.print(i+1 + "회차 - ");
+			System.out.print((i+1) + "회차 - ");
 			for(int k = 0 ; k < diceList[i].length ; k++) {
 				diceList[i][k] = r.nextInt(6) + 1;
-				System.out.print(k+1 + "번 : " + diceList[i][k] + "\t");
+				System.out.print((k+1) + "번 : " + diceList[i][k] + "\t");
 				total += diceList[i][k];
+				totalList[i] += diceList[i][k];
 				count++;
 			}
-			double roundAvg = (double)roundTotal / diceList[i].length;
-			System.out.print(i+1 + "회차의 평균 : " + roundAvg);
+			avgList[i] = (float)totalList[i] / diceList[i].length;
+			System.out.print((i+1) + "회차의 평균 : " + avgList[i]);
 			System.out.println();
 		}
 		double avg = (double)total / count;

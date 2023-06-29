a31d70b0122ccbb89e459ae641f74dad297aae66
wkdwns0501 <britain0501@naver.com>
2023-06-29 14:59:30 +0900

    2차원 배열 설명

---
+++ day09/src/array3/Test02.java
@@ -0,0 +1,27 @@
+package array3;
+
+public class Test02 {
+	public static void main(String[] args) {
+		//2차원 배열의 생성과 사용 //2차원은 1차원을 묶어논것이다
+		
+		int[][] score = new int[2][3]; //3개씩 2번 묶여있다
+		//score ---> score[0] ---> [0][0][0]
+		//			---> score[1] ---> [0][0][0]  //실제로는 가로 일자로 저장됨
+		
+		//초기화
+		score[0][1] = 15;
+		score[1][0] = 20;
+		score[1][2] = 40;
+		//출력
+		//System.out.println(score); // 통합리모컨
+		//System.out.println(score[0]); // 통합리모컨   // 의미없는 출력값이 나옴
+		
+		System.out.println(score[0][0]);
+		System.out.println(score[0][1]);
+		System.out.println(score[0][2]);
+		//System.out.println(score[0][3]); //에러, 범위초과
+		System.out.println(score[1][0]);
+		System.out.println(score[1][1]);
+		System.out.println(score[1][2]);
+	}
+}

de66f9ed655958b1eb4ea4e58a3ee0b360f68615
wkdwns0501 <britain0501@naver.com>
2023-06-29 10:00:24 +0900

    day08 과제 풀이 설명

--- day08/src/array/Test13.java
+++ day08/src/array/Test13.java
@@ -9,12 +9,17 @@ public class Test13 {
 		//1. 모든 자리는 1번씩 다른 위치와 바뀌어야 한다
 		//2. 어디랑 섞일지 대상 위치는 모르며 같은 위치일 수도 있다
 		//3. 랜덤으로 정해진 위치와 현재 위치의 데이터를 바꾸도록 구현
+		//4. 같은 자리 제거 , 같은자리가 나오면 다시 뽑아라, 같은자리가 나오면 하던 작을 무효 황 시켜라
 		Random r = new Random();
 		
 		int []card = new int[] {30,50,20,10,40};
 		
 		for (int i = 0 ; i < card.length ; i++) {
 			int num = r.nextInt(card.length);
+			if (i == num) {
+				i--;
+				continue;
+			}
 			int backup = card[i];
 			card[i] = card[num];
 			card[num] = backup;

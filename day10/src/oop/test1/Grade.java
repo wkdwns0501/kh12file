94cac97daac59bc292978b89f44f4061479b211b
wkdwns0501 <britain0501@naver.com>
2023-06-30 10:21:46 +0900

    Grade 클래스 생성

---
+++ day10/src/oop/test1/Grade.java
@@ -0,0 +1,11 @@
+package oop.test1;
+
+//시험 성적 정보를 자바에게 설명하기 위해 만드는 도구 (클래스)
+public class Grade { //가방
+	//시험 성적 정보의 구성요소 데이터를 정의 (멤버 필드)
+	//아래의 변수 4개를 묶어서 Grade라고 하겠다 (클래스 생성)
+	String name;
+	int korean;
+	int english;
+	int math;
+}

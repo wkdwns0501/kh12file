3315bef8ae117feb97272b23aa9a7c43ea496db8
wkdwns0501 <britain0501@naver.com>
2023-07-04 11:14:09 +0900

    객체생성이 필요없을때 - 2

---
+++ day12/src/oop/keyword3/Test01.java
@@ -0,0 +1,8 @@
+package oop.keyword3;
+
+public class Test01 {
+	public static void main(String[] args) {
+		int result = Calculator.plus(10, 20);
+		System.out.println("result = " + result);
+	}
+}

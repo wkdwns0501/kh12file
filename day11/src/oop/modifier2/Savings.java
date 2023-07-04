dc0d2a175f684882e58567e39438aab1dc706f3e
wkdwns0501 <britain0501@naver.com>
2023-07-04 10:19:25 +0900

    day11 과제 - 적금문제풀이

--- day11/src/oop/modifier2/Savings.java
+++ day11/src/oop/modifier2/Savings.java
@@ -1,10 +1,9 @@
 package oop.modifier2;
 
 public class Savings {
-	private String name;
-	private int period, count, money;
-	private long bal;
-	//this 필드의 값
+	private String name; //예금주명
+	private int period, count, money, bal; //총기간, 진행된 입금회차, 1회 입금액, 잔액
+	//this가붙으면 필드의 값, 안붙으면 바로 위에값
 	void setName(String name) {
 		this.name = name;
 	}
@@ -25,48 +24,61 @@ public class Savings {
 		return period;
 	}
 	public void setCount(int count) {
-		if(count < 0) return;
+		if(count < 1) return;
+		if(count >this.getTotal()) return;
 		else this.count = count;
 	}
 	public int getCount() {
 		return count;
 	}
 	public void setMoney(int money) {
-		if (money % 10000 != 0) return;
-		else this.money = money;
+		if (money <= 0) return;
+		this.money = money;
 	}
 	public long getMoney() {
 		return money;
 	}
-	public void setBal(long bal) {
-		if(bal < 0) return;
+	public void setBal(int bal) {
+		if(bal <= 0) return;
 		else this.bal = bal;
 	}
-	public long getBal() {
+	public int getBal() {
 		return bal;
 	}
-	public String getFin() {
-		if(this.period * 12 < this.count) return " (만기)";
-		else return "";
+	//만기인지 아닌지 판정하는 메소드
+	//(참고) 논리가 반환값인 Getter 메소드 get이 아니라 is로 시작하게 작명
+	public boolean isFin() {
+//		if(this.getTotal() == this.getCount()) return true;
+//		else return false;
+		return this.getTotal() == this.getCount();
 	}
-	public Savings(String name, int period, int money, long bal) {
-		this(name, period, 1, money, bal);
+	public int getMonth() {
+		return this.period * 12;
 	}
-	public Savings(String name, int period, int count, int money, long bal) {
+	public int getTotal() {
+		return this.getMonth() + 1;
+	}
+	
+	public Savings(String name, int period, int money) {
+		this(name, period, money, 1, 0);
+	}
+	public Savings(String name, int period, int money, int count, int bal) {
 		this.setName(name);
 		this.setPeriod(period);
-		this.setCount(count);
 		this.setMoney(money);
+		this.setCount(count);
 		this.setBal(bal);
 	}
 	
 	public void show() {
 		System.out.println("<KH 내집마련 적금통장 가입자 정보>");
 		System.out.println("예금주 이름 : " + this.getName());
-		System.out.println("총납입 기간 : " + this.getPeriod() + "년");
-		System.out.println("현재납입 회차 : " + this.getCount() + "회차" + this.getFin());
-		System.out.println("월납입 금액 : " + this.getMoney() + "원");
-		System.out.println("총잔액 : " + this.getBal() + "원");
-		
+		System.out.println("예치 기간 : " + this.getPeriod() + "년 (" + this.getMonth() + "개월)");
+		System.out.println("현재납입 회차 : (" + this.getCount() + " / " + this.getTotal() + ")");
+		System.out.println("월부금 : " + this.getMoney() + "원");
+		System.out.println("계좌 잔액 : " + this.getBal() + "원");
+		if(this.isFin()) {
+			System.out.println("* 해당 적금은 만기되었습니다. *");
+		}
 	}
 }
--- day11/src/oop/modifier2/Test01.java
+++ day11/src/oop/modifier2/Test01.java
@@ -3,9 +3,9 @@ package oop.modifier2;
 public class Test01 {
 	public static void main(String[] args) {
 		
-		Savings a = new Savings("유재석", 3, 15, 1000000, 15000000L);
-		Savings b = new Savings("강호동", 2, 5, 500000, 2500000L);
-		Savings c = new Savings("신동엽", 2, 8, 800000, 10000000L);
+		Savings a = new Savings("유재석", 3, 1000000, 15, 15000000);
+		Savings b = new Savings("강호동", 2, 500000, 5, 2500000);
+		Savings c = new Savings("신동엽", 2, 800000, 8, 10000000);
 		
 		a.show();
 		b.show();

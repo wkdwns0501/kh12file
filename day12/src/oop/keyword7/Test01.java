8919b784cabb8d0a6a629511391e867b934f738c
wkdwns0501 <britain0501@naver.com>
2023-07-04 16:26:11 +0900

    수정

--- day12/src/oop/keyword7/Test01.java
+++ day12/src/oop/keyword7/Test01.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class Test01 {
 	//어디서나 객체없이(static) 변하지 않는(final) 값을 만들어서 사용 = 상수
-	static final int scissors = 0;
-	static final int rock = 1;
-	static final int paper = 2;
+//	static final int scissors = 0;
+//	static final int rock = 1;
+//	static final int paper = 2;
 	
 	public static void main(String[] args) {
 		//상수(constant)의 필요성

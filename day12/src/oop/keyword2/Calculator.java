d23cfd92c0c181e9dd52567e5b28d4d91b2eafba
wkdwns0501 <britain0501@naver.com>
2023-07-04 11:13:55 +0900

    객체생성이 필요없을때 - 1

---
+++ day12/src/oop/keyword2/Calculator.java
@@ -0,0 +1,12 @@
+package oop.keyword2;
+
+//문제점
+//[1] 내부 구조가 복잡함 - 필드를 제거
+//[2] 객체를 꼭 만들어야함
+public class Calculator {
+	
+	public int plus(int left, int right) {
+		return left + right; // 1번 해결방법
+	}
+	
+}
---
+++ day12/src/oop/keyword2/Test01.java
@@ -0,0 +1,9 @@
+package oop.keyword2;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Calculator a = new Calculator();
+		int result = a.plus(10, 20);
+		System.out.println("result = " + result);
+	}
+}

a874a7ee1e936f51483107ad01adeb9c6a2b606d
wkdwns0501 <britain0501@naver.com>
2023-07-05 10:16:28 +0900

    상속이 필요한 이유

---
+++ day13/src/oop/inherit1/IPhone13.java
@@ -0,0 +1,20 @@
+package oop.inherit1;
+
+public class IPhone13 {
+	//필드
+	private String number; //전화번호
+	private String color; //색상
+	private int price; //가격
+	
+	//메소드 - 기능
+	//setter/getter 생략
+	public void call() {
+		System.out.println("전화걸기");
+	}
+	public void camera() {
+		System.out.println("사진찍기");
+	}
+	public void siri() {
+		System.out.println("시리야");
+	}
+}
---
+++ day13/src/oop/inherit1/IPhone14.java
@@ -0,0 +1,20 @@
+package oop.inherit1;
+
+public class IPhone14 {
+	//필드
+	private String number;
+	private String color;
+	private int price;
+	
+	//메소드 - 기능
+	//setter/getter 생략
+	public void call() {
+		System.out.println("전화걸기");
+	}
+	public void camera() {
+		System.out.println("사진찍기");
+	}
+	public void facetime() {
+		System.out.println("영상통화");
+	}
+}

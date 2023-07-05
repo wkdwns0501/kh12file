71c84809eaac10414584eb02155f1d27ee34ad28
wkdwns0501 <britain0501@naver.com>
2023-07-05 18:20:35 +0900

    day13 과제

---
+++ day13/src/oop/inherit8/Test02.java
@@ -0,0 +1,15 @@
+package oop.inherit8;
+
+public class Test02 {
+	public static void main(String[] args) {
+		Warrior a = new Warrior("테스터1");
+//		Magician a = new Magician("테스터1");
+//		Archer a = new Archer("테스터1");
+		
+		Warrior b = new Warrior("테스터2");
+//		Magician b = new Magician("테스터2");
+//		Archer b = new Archer("테스터2");
+		
+		Interaction.battle(a,b);
+	}
+}

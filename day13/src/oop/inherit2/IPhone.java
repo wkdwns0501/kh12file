562740195ca8da4e398513d9251c55d2a2199648
wkdwns0501 <britain0501@naver.com>
2023-07-05 10:27:34 +0900

    수정

--- day13/src/oop/inherit2/IPhone.java
+++ day13/src/oop/inherit2/IPhone.java
@@ -4,8 +4,8 @@ package oop.inherit2;
 //- 객체 생성이 목적이 아님
 //- 비슷한 클래스들의 공통적인 부분들을 모아두는 클래스
 //- 시리즈로 만들어야 할때 사용
-//- 80%~90% 똑같은게 있을때 사용
-//- IPhone 클래스가 this, 각각의 시리즈 클래스가 super
+//- 80%~90% 같은것이 있을때 사용
+//- IPhone 클래스가 super , 각각의 시리즈 클래스가 this
 //- 먼저 this에서 확인후 없으면 super로 넘어간다
 
 public class IPhone {

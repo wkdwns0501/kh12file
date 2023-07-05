520a7f128620268102ab032e1b74fe9bad0b49cf
wkdwns0501 <britain0501@naver.com>
2023-07-05 11:05:25 +0900

    수정

--- day13/src/oop/inherit3/Test01.java
+++ day13/src/oop/inherit3/Test01.java
@@ -2,18 +2,22 @@ package oop.inherit3;
 
 public class Test01 {
 	public static void main(String[] args) {
+		//Browser는 생성하면 안된다
 		ChromeBrowser a = new ChromeBrowser();
+		a.setUrl("https://www.google.com");
 		a.refresh();
 		a.move();
 		a.develop();
 		a.choromeStore();
 		
 		EdgeBrowser b = new EdgeBrowser();
+		a.setUrl("https://www.google.com");
 		b.refresh();
 		b.move();
 		b.fullScreen();
 		
 		WhaleBrowser c = new WhaleBrowser();
+		a.setUrl("https://www.google.com");
 		c.refresh();
 		c.move();
 		c.papago();

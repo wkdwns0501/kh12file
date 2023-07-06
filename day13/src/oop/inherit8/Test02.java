7526e42da58162d2f3f1df8509ab8917db5e5c69
wkdwns0501 <britain0501@naver.com>
2023-07-06 11:23:18 +0900

    day13 과제 풀이

--- day13/src/oop/inherit8/Test02.java
+++ day13/src/oop/inherit8/Test02.java
@@ -2,13 +2,13 @@ package oop.inherit8;
 
 public class Test02 {
 	public static void main(String[] args) {
-//		Warrior a = new Warrior("테스터1");
-//		Magician a = new Magician("테스터1");
-		Archer a = new Archer("테스터1");
+//		Player a = new Warrior("테스터1");
+//		Player a = new Magician("테스터1");
+		Player a = new Archer("테스터1");
 		
-//		Warrior b = new Warrior("테스터2");
-		Magician b = new Magician("테스터2");
-//		Archer b = new Archer("테스터2");
+//		Player b = new Warrior("테스터2");
+		Player b = new Magician("테스터2");
+//		Player b = new Archer("테스터2");
 		
 		Interaction.battle(a,b);
 	}

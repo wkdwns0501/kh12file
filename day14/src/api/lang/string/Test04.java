4cbf267c64ba223719218c7cd599e72d6a91ca6c
wkdwns0501 <britain0501@naver.com>
2023-07-07 09:21:18 +0900

    day14 과제

---
+++ day14/src/api/lang/string/Test04.java
@@ -0,0 +1,33 @@
+package api.lang.string;
+
+import java.util.Scanner;
+
+public class Test04 {
+	public static void main(String[] args) {
+//		사용자에게 프로그램 내에서 사용할 닉네임을 입력받도록 구현
+//		단, 다음과 같은 상황일 경우 오류 메세지를 출력
+//		닉네임이 2글자 이상 10글자 이하가 아닌 경우
+//		닉네임에 운영자라는 단어가 포함된 경우
+//		모든 검사를 통과했다면 닉네임 설정이 완료되었습니다라는 메세지를 출력
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("닉네임 입력 : ");
+		String nick = sc.next();
+		sc.close();
+		
+		boolean length = nick.length() >= 2 && nick.length() <= 10;
+		boolean word = nick.contains("운영자");
+		
+		if ( !length ) {
+			System.out.println("글자수는 2글자 이상 10글자 이하만 가능합니다");
+		}
+		else if ( word ) {
+			System.out.println("운영자라는 단어를 포함할 수 없습니다");
+		}
+		else {
+			System.out.println("닉네임 설정이 완료되었습니다");
+		}
+		
+		
+	}
+}

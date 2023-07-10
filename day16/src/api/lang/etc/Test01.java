2544726c2d3c7db067f5a556a73170ce35f9feab
wkdwns0501 <britain0501@naver.com>
2023-07-10 12:41:52 +0900

    특이한 클래스들

---
+++ day16/src/api/lang/etc/Test01.java
@@ -0,0 +1,18 @@
+package api.lang.etc;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//생성자가 없는 클래스
+		//= 클래스는 객체 생성이 주 목적이다
+		//= 생성자가 없다는 것은 생성자가 private란 소리이다
+		//= 생성자가 없다는 것은 객체를 만들 수 없다는 의미
+		
+		//[1] 객체를 아예 못만들게 하려는 목적
+		//[2] 객체를 만들어주는 메소드를 제공하려는 목적(통제)
+		
+//		Math a = new Math();
+//		System b = new System();
+//		Runtime c = new Runtime();
+//		Integer d = new Integer(10);
+	}
+}

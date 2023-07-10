285839a6105bfe6e76529c149e465cf01e05947c
wkdwns0501 <britain0501@naver.com>
2023-07-10 21:15:15 +0900

    day15 과제 수정

--- day15/src/api/lang/string2/Test07.java
+++ day15/src/api/lang/string2/Test07.java
@@ -9,21 +9,11 @@ public class Test07 {
 		String memberId = sc.next();
 		String memberPw = sc.next();
 		String memberName = sc.next();
-<<<<<<< HEAD
-		System.out.print("등급 : ");
-		String memberLevel = sc.next();
-		System.out.print("포인트 : ");
-		String memberPoint = sc.next();
-		sc.close();
-=======
 		sc.close();
 		
 		Member a = new Member(memberId,memberPw,memberName);
 		a.show();
->>>>>>> branch 'main' of https://github.com/wkdwns0501/kh12file.git
 		
-		Member a = new Member(memberId, memberPw, memberName,memberLevel,100);
 		
-		a.show();
 	}
 }

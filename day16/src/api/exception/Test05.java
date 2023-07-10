45e6b5aac5daecf1d0f12a4be35c98e7c2f109b5
wkdwns0501 <britain0501@naver.com>
2023-07-10 17:42:24 +0900

    강제 예외 처리와 예외 메세지 작성법

--- day16/src/api/exception/Test05.java
+++ day16/src/api/exception/Test05.java
@@ -2,18 +2,25 @@ package api.exception;
 
 import java.util.Scanner;
 
-public class Test04 {
+public class Test05 {
 	public static void main(String[] args) {
-		//Test02 예제에서 예외를 한 번에 처리하도록 변경
+		//Test04에서 자바가 감지하지 못하는 예외를 개발자가 수동으로 처리
+		//-->강제 예외 처리
 		try {//플랜A
 			Scanner sc = new Scanner(System.in);
 			
 			System.out.print("금액 : ");
 			int money = sc.nextInt();
+			if(money < 0) {//자바가 인지하지 못하는 문제 상황
+				Exception ex = new Exception("금액이 음수일 수 없습니다");
+				throw ex; //throw를 이용하면 실행을 중지하고 catch블록으로 이동한다
+			}
 			
 			System.out.print("인원 : ");
 			int people = sc.nextInt();
-			
+			if(people < 0) {//자바가 인지하지 못하는 문제 상황
+				throw new Exception("인원수는 음수일 수 없습니다"); //15,16 행이랑 같은 코드이다 
+			}																				//보통 이 코드를 쓴다
 			sc.close();
 			
 			int price = money / people; //1인당 정산금액
@@ -26,7 +33,19 @@ public class Test04 {
 //		catch (Throwable e)				
 //		catch (Object e) //안됨         
 		catch (Exception e){ //플랜B. 무언가 문제가 생겼을 때
-			System.err.println("에러!");
+			//System.err.println("에러!");
+			
+			//합쳐놓고 보니 자세한 정보가 그립더라 (예외 객체 활용)
+			//System.out.println(e);
+			//System.out.println(e instanceof ArithmeticException);
+			
+			//예외 객체에 설정된 메세지가 있다면 출력할 수 있다
+			if(e.getMessage() == null) {
+				System.err.println("오류 발생");
+			}
+			else {
+				System.err.println(e.getMessage());
+			}
 		}
 		//Error는 실행자체가 안되는것 - 개발자 잘못 , 에러를 고치면 안된다
 		//Exception는 실행은 되지만 오류가 나는것 - 실행자 잘못, 에러를 고쳐야 한다

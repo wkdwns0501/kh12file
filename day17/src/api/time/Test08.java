64b9df0d070da8fb2a166889ddaf994800e0591c
wkdwns0501 <britain0501@naver.com>
2023-07-11 16:04:51 +0900

    LocalDate 예제

---
+++ day17/src/api/time/Test08.java
@@ -0,0 +1,56 @@
+package api.time;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Test08 {
+	public static void main(String[] args) {
+		//java.time 패키지 (Java 8+)
+		//- 완전히 새롭게 구축된 시간 관련된 클래스 모음
+		//- 기존의 시간 클래스들은 날짜와 시간을 따로 생각할 수 없다
+		//- 날짜만 쓰고 싶으면 LocalDate를 써라!
+		//- 시간만 쓰고 싶으면 LocalTime을 써라!
+		//- 날짜와 시간이 다 필요하면 LocalDateTime을 써라!
+		//- 월을 1~12로 관리하도록 개선
+		//- 명령을 단순화함
+		//- 불변으로 관리하도록 설정(=문자열처럼) = 다중쓰레드 환경에서 안전
+		
+		//LocalDate 사용
+		LocalDate a = LocalDate.of(2000, 1, 1); //수치
+		System.out.println(a);
+		System.out.println(a.getYear()); //년도만 뽑기
+		System.out.println(a.getMonthValue()); //월을 숫자로 뽑기 getMonth는 영어로 나온다
+		System.out.println(a.getDayOfMonth()); //일만 뽑기
+		
+		LocalDate b = LocalDate.parse("2023-11-29"); //해석(변환)
+		System.out.println(b);
+		
+		LocalDate c = LocalDate.now(); //지금
+		System.out.println(c);
+		
+		//이전/이후 비교
+		//= 수료 했습니까?
+		//= 오늘은 수료일보다 이전입니까?
+		System.out.println(c.isBefore(b));
+		System.out.println(b.isAfter(c));
+		
+		//윤년
+		System.out.println(a.isLeapYear());
+		System.out.println(b.isLeapYear());
+		System.out.println(c.isLeapYear());
+		
+		//계산
+		//(Q) 수료일 100일 전은 언제입니까?
+		LocalDate before100 = b.minusDays(100L);
+		System.out.println(before100);
+		
+		//두 날짜의 차이 (ex : 오늘과 수료일의 차이)
+		//= 유틸리티 클래스인 Period를 사용하여 차이를 구한다
+		
+		Period period = Period.between(c,b);
+		System.out.println(period);
+		System.out.println(period.getYears()); //남은기간 (연)
+		System.out.println(period.getMonths()); //남은기간 (월)
+		System.out.println(period.getDays()); //남은기간 (일)
+	}
+}

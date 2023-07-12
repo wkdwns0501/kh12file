e169291ec5a2fdcb1d151304c8569ca232bc6269
wkdwns0501 <britain0501@naver.com>
2023-07-12 18:00:39 +0900

    추가 문제

---
+++ day18/src/api/collection/advance.java
@@ -0,0 +1,17 @@
+package api.collection;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class advance {
+	public static void main(String[] args) {
+		//추가문제
+		
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> figure = new ArrayList<>();
+		ArrayList<String> character = new ArrayList<>();
+	
+		System.out.print("인원수 입력 (2~4명) : ");
+		int num = sc.nextInt();
+	}
+}

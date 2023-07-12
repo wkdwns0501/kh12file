e172a46ed8b918cdd19ce0fc657a06dbcdc9bb9b
wkdwns0501 <britain0501@naver.com>
2023-07-12 18:14:33 +0900

    설명 추가

--- day18/src/api/collection/Test09.java
+++ day18/src/api/collection/Test09.java
@@ -13,6 +13,8 @@ public class Test09 {
 		//- LinkedList는 데이터가 연결 형태로 존재하는 리스트
 		//		- 조회 성능이 다소 떨어짐 (ArrayList에 비해)
 		//		- 데이터 구조 변경 성능이 우수함
+		//- Vector는 체계가 덜 잡힌 과거 자바에서 사용하던 클래스
+		//		- 조회 성능이 떨어져서 ArrayList로 교체하여 사용
 		//- a + List : a특징을 가진 List
 		
 		List<String> a = new ArrayList<>();

6f2234e1f0390013394bfe1d1d9cdb9cb6a5c54c
wkdwns0501 <britain0501@naver.com>
2023-07-14 10:49:22 +0900

    day19 과제

--- day19/src/api/collection3/Test03.java
+++ day19/src/api/collection3/Test03.java
@@ -8,11 +8,24 @@ public class Test03 {
 	public static void main(String[] args) {
 		Map<String, Integer> vote = new HashMap<>();
 		Scanner sc = new Scanner(System.in);
-		System.out.print("이름 입력 : ");
-		String name = sc.next();
-		int count = 0;
 		while(true) {
-			vote.put(name,count);
+			System.out.print("이름 입력 : ");
+			String name = sc.next();
+			if(name.equals("종료")) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
+			if(vote.containsKey(name)) {
+				int count = vote.get(name);
+				vote.put(name,count+1);
+				System.out.println("[" + name + "] 현재 " +(count+1)+ "표 획득!");
+			}
+			else {
+				vote.put(name, 1);
+				System.out.println("[" + name + "] 현재 " +1+ "표 획득");
+			}
 		}
+		sc.close();
+		System.out.println(vote);
 	}
 }

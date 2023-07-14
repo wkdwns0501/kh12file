2b38d8c92788be5c986b95817b2adfe270cf9346
wkdwns0501 <britain0501@naver.com>
2023-07-14 12:44:01 +0900

    디렉터리 분석 코드 추가

--- day20/src/api/file/Test01.java
+++ day20/src/api/file/Test01.java
@@ -33,5 +33,22 @@ public class Test01 {
 			System.out.println(a.length());//파일의 크기 (=들어있는 글자의 개수)
 			System.out.println(a.lastModified());//최종 수정 시간 //밀리초로 나온다
 		}
+		
+		//디렉터리도 File 클래스로 제어한다
+		File c = new File("D:/");
+		File d = new File("D:\\");
+		
+		if(c.isDirectory()) {
+			System.out.println(c.getName());
+			System.out.println(c.getPath());
+			System.out.println(c.length());//(주의) 디렉터리의 크기는 의미가 없다
+			
+			//디렉터리는 내부에 존재하는 요소들을 추출할 수 있다
+			String[] names = c.list(); //이름만 추출
+			File[] files = c.listFiles(); //파일 객체를 추출 (이름 + 나머지정보)
+			for(File file : files) {
+				System.out.println(file);
+			}
+		}
 	}
 }

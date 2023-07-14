ad8d72bfd75ea0bac4fb503d9b19e1c7f3306361
wkdwns0501 <britain0501@naver.com>
2023-07-14 18:19:32 +0900

    day20 과제

---
+++ day20/src/api/file/single2/Test03.java
@@ -0,0 +1,40 @@
+package api.file.single2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
+public class Test03 {
+	public static void main(String[] args) throws IOException {
+		//기존에 만든 파일 복사 프로그램을 byte[]을 이용하여 처리하도록 변경
+		//최적의 버퍼 크기를 찾아서 설정
+		
+		File readTarget = new File("D:/assign.txt");
+		FileInputStream stream1 = new FileInputStream(readTarget);
+		
+		File writeTarget = new File("./sample/copy2.txt");
+		FileOutputStream stream2 = new FileOutputStream(writeTarget);
+		
+		long count = 0L;
+		long total = readTarget.length();
+		DecimalFormat fmt = new DecimalFormat("#,##0.00");
+		byte[] buffer = new byte[30];
+		
+		while(true) { 
+			int size = stream1.read(buffer);
+			if(size == -1) break;
+			stream2.write(buffer);
+//			System.out.print(size);
+//			System.out.print("\t");
+//			System.out.println(Arrays.toString(buffer));
+			count++;
+			double percent = count * 100d / total;
+			System.out.println(count + "/" + total + "("+fmt.format(percent)+"%)");
+		}
+		stream1.close();
+		stream2.close();
+	}
+}

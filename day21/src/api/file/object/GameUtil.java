5deed3716b812ce42931fbf723c4d61ac96fd6d1
wkdwns0501 <britain0501@naver.com>
2023-07-17 18:13:21 +0900

    파일 입출력을 활용한 게임문제

---
+++ day21/src/api/file/object/GameUtil.java
@@ -0,0 +1,12 @@
+package api.file.object;
+
+public class GameUtil {
+	//캐릭터 불러오기
+	public static Player load() {
+		
+	}
+	//캐릭터 저장하기
+	public static void save(Player p) {
+		
+	}
+}
---
+++ day21/src/api/file/object/Player.java
@@ -0,0 +1,47 @@
+package api.file.object;
+
+import java.io.Serializable;
+
+//캐릭터 클래스
+//= 입출력이 가능하도록 마킹 처리
+public class Player implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private int level;
+	private long money;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		if(level < 1) return;
+		this.level = level;
+	}
+	public long getMoney() {
+		return money;
+	}
+	public void setMoney(long money) {
+		if(money <0) return;
+		this.money = money;
+	}
+	public void levelUp() {
+		this.level ++;
+	}
+	public Player(String id) {
+		this.setId(id);
+		this.setLevel(1);
+		this.setMoney(0);
+	}
+	public void show() {
+		System.out.println("<캐릭터 정보>");
+		System.out.println("아이디 : " + this.id);
+		System.out.println("레벨 : " + this.level);
+		System.out.println("소지금 : " + this.money + " gold");
+	}
+	
+}

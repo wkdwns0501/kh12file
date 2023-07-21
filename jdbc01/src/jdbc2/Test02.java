e9ebff6791859f3e4fb2576b33608b011bee89d4
wkdwns0501 <britain0501@naver.com>
2023-07-21 18:20:40 +0900

    day25 과제

---
+++ jdbc01/src/jdbc2/Test02.java
@@ -0,0 +1,40 @@
+package jdbc2;
+
+import java.util.Scanner;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class Test02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("아이디 : ");
+		int bookId = sc.nextInt();
+		System.out.print("제목 : ");
+		String bookTitle = sc.next();
+		System.out.print("가격 : ");
+		float bookPrice = sc.nextFloat();
+		sc.close();
+		
+		String sql = "update book set book_title=?, book_price=? where book_id = ?";
+		Object[] data = {bookTitle, bookPrice, bookId};
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver"); 
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe"); 
+		dataSource.setUsername("C##KH");
+		dataSource.setPassword("KH"); 
+		
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		
+		int result =jdbcTemplate.update(sql, data);
+		System.out.println("result = " + result); 
+		
+		if(result>0) {
+			System.out.println("수정 성공!");
+		}
+		else {
+			System.out.println("수정 실패!");
+		}
+	}
+}
---
+++ jdbc01/src/jdbc2/Test03.java
@@ -0,0 +1,11 @@
+package jdbc2;
+
+import java.util.Scanner;
+
+public class Test03 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String aql = "update board set "
+	}
+}

0fec7362107a23d53e5f7aea0aa66223677477b9
wkdwns0501 <britain0501@naver.com>
2023-07-25 15:11:09 +0900

    포켓몬스터 조회/상세 구현

--- jbdc03/src/jdbc/dao/PocketmonDao.java
+++ jbdc03/src/jdbc/dao/PocketmonDao.java
@@ -1,8 +1,12 @@
 package jdbc.dao;
 
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import jdbc.dto.PocketmonDto;
+import jdbc.mapper.PocketmonMapper;
 import jdbc.util.JdbcUtils;
 
 //DAO 클래스
@@ -47,4 +51,49 @@ public class PocketmonDao {
 		//위코드와 같음
 		return result>0;					//result가 0보다 큰지 판정해서 반환해라
 	}
+	
+	//D(삭제) 메소드
+	//- 삭제 모소드는 Primary key(기본키)를 이용하여 하나씩 지운다
+	//- 조건에 따라서 삭제가 안될 수 있기 때문에 결과를 반환해야 한다
+//	public boolean delete(PocketmonDto dto){
+	public boolean delete(int no) {
+		String sql = "delete pocketmon where no=?";
+		Object[] data = {no};
+		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+		return jdbcTemplate.update(sql,data) > 0;
+	}
+	
+	//Mapper는 외부에 필드로 생성
+	private PocketmonMapper mapper = new PocketmonMapper();
+	
+	//R(조회) 메소드
+	//- 여러 개의 데이터가 나오는 구문
+	//- 자바에서는 List<DTO> 형태로 취급
+	//- 조회하려면 Mapper가 필요
+	public List<PocketmonDto> selectList() {
+		String sql = "select * from pocketmon";
+		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+//		List<PocketmonDto> list = jdbcTemplate.query(sql, mapper);
+//		return list;
+		return jdbcTemplate.query(sql, mapper);
+	}
+	//R(상세) 메소드
+	//- Primary Key를 이용하여 하나의 결과만 나오는 구문
+	//- 자바에서는 DTO 형태로 취급
+	//- 조회하려면 Mapper가 필요
+//	public PocketmonDto selecOne(PocketmonDto dto) {
+	public PocketmonDto selectOne(int no) {
+		String sql = "select * from pocketmon where no=?";
+		Object[] data = {no};
+		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+		List<PocketmonDto> list = jdbcTemplate.query(sql, mapper, data);
+//		if(list.isEmpty()) {
+//			return null;
+//		}
+//		else {
+//			return list.get(0);
+//		}
+		return list.isEmpty() ? null : list.get(0);
+	}
+	
 }

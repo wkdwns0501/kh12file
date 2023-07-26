c51474d798d0b3c812f3a75c746fb24915a53241
wkdwns0501 <britain0501@naver.com>
2023-07-26 14:57:16 +0900

    로또 리스트 추가

--- spring03controller/src/main/java/com/kh/spring03/controller/QuizController.java
+++ spring03controller/src/main/java/com/kh/spring03/controller/QuizController.java
@@ -1,5 +1,8 @@
 package com.kh.spring03.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,17 @@ public class QuizController {
 	public String lotto() {
 		int lotto = r.nextInt(45)+1;
 		return "로또번호 : " + lotto;
+	}
+	
+	@RequestMapping("/lotto-list")
+	public List<Integer> lottoList() {
+		List<Integer> list = new ArrayList<>();
+		for(int i=1; i <= 45;i ++) {
+			list.add(i);
+		}
+		Collections.shuffle(list);
+		List<Integer> lotto = list.subList(0, 6);
+		Collections.sort(lotto);
+		return lotto;
 	}
 }

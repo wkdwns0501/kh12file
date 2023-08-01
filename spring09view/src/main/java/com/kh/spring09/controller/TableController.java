23f07fa9320d2dec9c152bfad4a6cc673a495373
wkdwns0501 <britain0501@naver.com>
2023-08-01 13:02:57 +0900

    테이블 기본 생성 예제

---
+++ spring09view/src/main/java/com/kh/spring09/controller/TableController.java
@@ -0,0 +1,13 @@
+package com.kh.spring09.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/table")
+public class TableController {
+	@RequestMapping("/test01")
+	public String test01() {
+		return"/WEB-INF/views/table/test01.jsp";
+	}
+}

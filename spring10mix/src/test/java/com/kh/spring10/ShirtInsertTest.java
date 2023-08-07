d1cd04f8efcc2665111d66a1b1cca47384ccf740
wkdwns0501 <britain0501@naver.com>
2023-08-07 19:40:28 +0900

    테스트 설명 추가

--- spring10mix/src/test/java/com/kh/spring10/ShirtInsertTest.java
+++ spring10mix/src/test/java/com/kh/spring10/ShirtInsertTest.java
@@ -7,13 +7,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.kh.spring10.dao.ShirtDao;
 import com.kh.spring10.dto.ShirtDto;
 
-@SpringBootTest
+//Spring 테스트 공간
+//- Spring에서는 개발중인 도구들 테스트 할 수 있도록 기능을 제공
+//- JUnit이라는 자바 테스팅 프레임워크를 가져다가 스프링화 시켜서 사용
+//- spring-boot-starter-test라는 의존성에서 모두 제공(pom.xml)
+
+@SpringBootTest//스프링에서 등록한 모든 도구를 가져다 쓸 수 있다
 public class ShirtInsertTest {
 	
 	@Autowired
 	private ShirtDao dao;
 	
-	@Test
+	@Test//JUnit을 이용하여 테스트를 할 코드를 작성하는 메소드
 	public void test() {
 		ShirtDto dto = new ShirtDto();
 		dto.setShirtName("테스트셔츠");

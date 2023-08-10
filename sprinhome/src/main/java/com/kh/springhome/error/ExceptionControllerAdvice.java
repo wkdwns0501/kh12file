accb0d990373ad3022e0867e0875ba9265888363
wkdwns0501 <britain0501@naver.com>
2023-08-10 11:02:24 +0900

    커스텀 예외 설정 및 페이지 구현

--- sprinhome/src/main/java/com/kh/springhome/error/ExceptionControllerAdvice.java
+++ sprinhome/src/main/java/com/kh/springhome/error/ExceptionControllerAdvice.java
@@ -3,8 +3,6 @@ package com.kh.springhome.error;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 컨트롤러를 간섭하여 예외 상황을 다르게 처리하도록 하는 객체
@@ -13,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * - 적용 대상을 옵션으로 지정
  */
 //@ControllerAdvice(basePackages = {"com.kh.springhome.controller"})
-@ControllerAdvice(annotations = {Controller.class})//대상명시(등록)
+@ControllerAdvice(annotations = {Controller.class})//대상명시(등록)//try 구문
 //ControllerAdvice의 주석처리로 ON/OFF가 가능하다
 public class ExceptionControllerAdvice {
 	/**
@@ -25,13 +23,27 @@ public class ExceptionControllerAdvice {
 	 * 		- Model, HttpSession 등 기본적인 컨트롤러의 도구 사용 가능
 	 * 		- catch 처럼 예외 처리 객체를 선언할 수 있다
 	 */
+	//catch 구문
 	@ExceptionHandler(Exception.class) //Exception 오류가 발생하면 여기로 불러옴
 	public String error(Exception e) {
 		e.printStackTrace(); //catch 구문에 있던 것 //콘솔에 찍힘
 		return "/WEB-INF/views/error/500.jsp"; //500 - 실행오류 //사용자 화면에 찍힘
 	}
 	
+	/**
+	 * 내가 지정한 예외들을 처리하도록 추가 핸들러(처리기) 설정
+	 */
+	@ExceptionHandler(NoTargetException.class)
+	public String noTarget(NoTargetException e) { //Exception e도 가능
+//		e.printStackTrace();
+		return "/WEB-INF/views/error/noTarget.jsp";
+	}
 	
+	@ExceptionHandler(AuthorityException.class)
+	public String authority(AuthorityException e) {
+		e.printStackTrace();
+		return "/WEB-INF/views/error/authority.jsp";
+	}
 	
 	
 	

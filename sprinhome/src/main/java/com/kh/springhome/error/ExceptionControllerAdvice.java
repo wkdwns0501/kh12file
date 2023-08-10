2e433f8605d9aad503f1a5bee876f5812d07e65c
wkdwns0501 <britain0501@naver.com>
2023-08-10 10:03:51 +0900

    예외 처리 도구 생성

--- sprinhome/src/main/java/com/kh/springhome/error/ExceptionControllerAdvice.java
+++ sprinhome/src/main/java/com/kh/springhome/error/ExceptionControllerAdvice.java
@@ -21,10 +21,14 @@ public class ExceptionControllerAdvice {
 	 * - 처리할 예외의 종류 지정 가능
 	 * - 메소드의 형태는 컨트롤러와 같음
 	 * - try 구문의 catch 블록과 같은 역할 수행
+	 * - 이 메소드에는ㄴ 컨트롤러처럼 매개변수를 선언하면 사용할 수 있다
+	 * 		- Model, HttpSession 등 기본적인 컨트롤러의 도구 사용 가능
+	 * 		- catch 처럼 예외 처리 객체를 선언할 수 있다
 	 */
 	@ExceptionHandler(Exception.class) //Exception 오류가 발생하면 여기로 불러옴
-	public String error() {
-		return "/WEB-INF/views/error/500.jsp"; //500 - 실행오류
+	public String error(Exception e) {
+		e.printStackTrace(); //catch 구문에 있던 것 //콘솔에 찍힘
+		return "/WEB-INF/views/error/500.jsp"; //500 - 실행오류 //사용자 화면에 찍힘
 	}
 	
 	

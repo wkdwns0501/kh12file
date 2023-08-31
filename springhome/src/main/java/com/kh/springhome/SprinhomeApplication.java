44dcc4a291e100bfa5d2c88c1b4cda3ed9bade36
wkdwns0501 <britain0501@naver.com>
2023-08-31 13:18:41 +0900

    오류 수정

---
+++ springhome/src/main/java/com/kh/springhome/SprinhomeApplication.java
@@ -0,0 +1,13 @@
+package com.kh.springhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinhomeApplication.class, args);
+	}
+
+}

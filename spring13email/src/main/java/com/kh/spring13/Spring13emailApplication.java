8b64bd4924fb1b7abc5cc398e172bb986da8eb52
wkdwns0501 <britain0501@naver.com>
2023-09-18 11:00:22 +0900

    spring13email 생성

---
+++ spring13email/src/main/java/com/kh/spring13/Spring13emailApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring13emailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring13emailApplication.class, args);
+	}
+
+}

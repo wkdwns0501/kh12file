9307bd921c7d388bdfb4649a93b7faf8f63d2fe6
wkdwns0501 <britain0501@naver.com>
2023-10-17 16:17:18 +0900

    이메일에 계산된 링크를 추가하여 발송 및 Multipart 첨부파일 메일 테스트

--- spring18security/src/main/java/com/kh/spring18/service/EmailServiceImpl.java
+++ spring18security/src/main/java/com/kh/spring18/service/EmailServiceImpl.java
@@ -1,17 +1,19 @@
 package com.kh.spring18.service;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,15 +32,32 @@ public class EmailServiceImpl implements EmailService{
 		helper.setSubject("[KH정보교육원] 가입을 축하합니다!");
 		
 		ClassPathResource resource = new ClassPathResource("templates/celebration.html");
-		File target = resource.getFile();
-		Scanner sc = new Scanner(target);
-		StringBuffer buffer = new StringBuffer();
-		while(sc.hasNextLine()) {
-			buffer.append(sc.nextLine());
-		}
-		sc.close();
+		Document doc = Jsoup.parse(resource.getFile(), "UTF-8");
 		
-		helper.setText(buffer.toString(), true);
+//		Scanner sc = new Scanner(target);
+//		StringBuffer buffer = new StringBuffer();
+//		while(sc.hasNextLine()) {
+//			buffer.append(sc.nextLine());
+//		}
+//		sc.close();
+		
+		//수신자 표시
+		Element target = doc.getElementById("target");
+		target.text(email);
+		
+		//로그인 링크 작성
+		Element link = doc.getElementById("login-link");
+//		link.attr("href", "/secure/login"); //절대경로지만 안됨 (다른사이트에서 실행하기 때문)
+		
+		//주소를 현재 상황에 맞게 생성하는 도구 사용
+		String href = ServletUriComponentsBuilder
+													.fromCurrentContextPath()//현재의 context path에서
+													.path("/secure/login")//세부 경로를 더하고
+													.build()//만든다음
+													.toUriString();//문자열로 바꿔라
+		link.attr("href", href);
+		
+		helper.setText(doc.toString(), true);
 		
 		sender.send(message);
 	}
